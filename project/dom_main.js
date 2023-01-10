b2bb63f1e2cdd4d114789151183403e476bbea2b
umesh <umesh2022del@gmail.com>
2023-01-10 13:52:54 +0600

    secondCommit

---
+++ project/dom_main.js
@@ -0,0 +1,10 @@
+var element;
+document.getElementById("menu").addEventListener("click",abc);
+
+function abc(){
+   var a= document.getElementById("menu").classList.contains("efg");
+ // var a=  document.getElementById ("menu").classList.length; 
+  console.log(a);
+
+}
+
